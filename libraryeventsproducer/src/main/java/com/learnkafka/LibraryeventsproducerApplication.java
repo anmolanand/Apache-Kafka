92e7a6ea3999ae091c00e1c2bd768af9398270c7
anmolanand <anmolanand@gmail.com>
2022-10-15 23:21:15 +0530

    Initial commit of structure of Apache Kafka Library events producer project.

---
+++ libraryeventsproducer/src/main/java/com/learnkafka/LibraryeventsproducerApplication.java
@@ -0,0 +1,13 @@
+package com.learnkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryeventsproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryeventsproducerApplication.class, args);
+	}
+
+}
